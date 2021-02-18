1ff4992d2b51f7243bf9070a405cd77dc418a66b
Santos Sabanari <santos.sabanari@yahoo.com>
2021-02-19 03:42:03 +0700

    update datatable

--- resources/views/components/includes/_options.blade.php
+++ resources/views/components/includes/_options.blade.php
@@ -1,9 +1,7 @@
 @if ($paginationEnabled || $searchEnabled)
     <div class="row mb-4">
         @if ($paginationEnabled && $perPageEnabled)
-            <div class="col form-inline">
-                {{ $perPageLabel }}: &nbsp;
-
+            <div class="col-4 col-md-6 form-inline">
                 <select wire:model="perPage" class="form-control">
                     @if (is_array($perPageOptions))
                         @foreach ($perPageOptions as $option)
@@ -11,15 +9,16 @@
                         @endforeach
                     @else
                         <option>10</option>
-                        <option>15</option>
                         <option>25</option>
+                        <option>50</option>
+                        <option>100</option>
                     @endif
                 </select>
             </div>
         @endif
 
         @if ($searchEnabled)
-            <div class="col">
+            <div class="col-8 col-md-6">
                 <input
                     @if (is_numeric($searchDebounce)) wire:model.debounce.{{ $searchDebounce }}ms="search"
                     @elseif($searchDebounce == false) wire:model.lazy="search"
